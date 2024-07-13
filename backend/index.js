cf49b2e57c574c76a79b13fd9bd12d1c9f7d7d12
Sajal <sajalchandra124@gmail.com>
2024-07-14 02:21:12 +0530

    backend commit routes not working in different directory

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express, { request } from "express"
 import {PORT,MongoDBURL} from "./config.js"
 import mongoose, { connect } from "mongoose";
 import {Book} from "./models/book-models.js"
+import {router} from "./routes/booksRoute.js"
 
 const app=express();
 
@@ -11,47 +12,7 @@ app.get('/',(req,res)=>{
     console.log(req);
     return res.status(234).send('Welcome to Sajals project')
 })
-
-// Below mentioned code is to create a GET endpoint to fetch all books, this was done purely for understanding purposes
-app.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find({});
-        res.status(200).send({
-            count: books.length,
-            data:books
-        });
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message });
-    }
-});
-
-app.post('/books',async (req,res)=>{
-    try{
-        if(  !req.body.title||
-            !req.body.author||
-            !req.body.publishYear
-        ){
-            return res.status(400).send({
-                message:'Send all required fields: title, author, publishYear',
-            });
-        }
-        const newBook={
-            title:req.body.title,
-            author:req.body.author,
-            publishYear:req.body.publishYear
-
-        };
-        const book=await Book.create(newBook); 
-
-        return res.status(201).send(book);
-    }
-    catch(error){
-        console.log(error.message) 
-        res.status(500).send({message:error.message})
-    }
-})
-
+app.use('/books',ksRoute)
 mongoose
 .connect(MongoDBURL) 
 .then(()=>{
