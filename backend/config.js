c978cd1f3409b1ec73f3c03a789ad713e49891cc
Sajal <sajalchandra124@gmail.com>
2024-07-16 19:53:12 +0530

    Backend Express routings working

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT=5555;
 
-export const MongoDBURL='mongodb+srv://sajal:root@book-store.egfu6t8.mongodb.net/?retryWrites=true&w=majority&appName=Book-Store'
+export const MongoDBURL = 'mongodb+srv://sajchan2501:ZYxWzNNnrxqd8m8d@book-store.mwz4f4c.mongodb.net/?retryWrites=true&w=majority&appName=book-store'
