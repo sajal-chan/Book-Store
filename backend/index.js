b2cbd5accba6f2fb6ff3c00f51006148fb5839dc
Sajal <sajalchandra124@gmail.com>
2024-07-17 11:06:55 +0530

    Backend Working

--- backend/index.js
+++ backend/index.js
@@ -14,107 +14,15 @@ app.get('/',(req,res)=>{
 })
 app.use('/books',router)
 
-// // Below mentioned code is to create a GET endpoint to fetch all books, this was done purely for understanding purposes
-// app.get('/books', async (req, res) => {
-//     try {
-//         const books = await Book.find({});
-//         res.status(200).send({
-//             count: books.length,//this displays all the number of books int he DB at once 
-//             data:books//we can just simpy put this here instead of the object
-//         });
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).send({ message: error.message });
-//     }
-// });
-
-// //This routing is to get a single book by its id
-// app.get('/books/:id', async (req, res) => {
-//     try {
-
-//         const { id }=req.params;
-
-//         const book = await Book.findById(id);
-//         res.status(200).json(book);
-//     } catch (error) {
-//         console.log(error.message);
-//         res.status(500).send({ message: error.message });
-//     }
-// });
-
-// //this route is to create a new book
-// app.post('/books',async (req,res)=>{
-//     try{
-//         if( !req.body.title||
-//             !req.body.author||
-//             !req.body.publishYear)
-//         {
-//             return res.status(400).send({
-//                 message:'Send all required fields: title, author, publishYear',
-//             });
-//         } 
-
-//         const newBook={
-//             title:req.body.title,
-//             author:req.body.author,
-//             publishYear:req.body.publishYear
-
-//         };
-//         const book=await Book.create(newBook); 
-
-//         return res.status(201).send(book);
-//     }
-//     catch(error){
-//         console.log(error.message) 
-//         res.status(500).send({message:error.message})
-//     }
-// });
-
-// //this route is to update an existing book
-// app.put('/books/:id',async(req,res)=>
-// {
-//     try{
-//         if( !req.body.title||
-//             !req.body.author||
-//             !req.body.publishYear)
-//         {
-//             return res.status(400).send({
-//                 message:'Send all required fields: title, author, publishYear',
-//             });
-//         } 
-
-//         const {id}=req.params;
-
-//         const result=await Book.findByIdAndUpdate(id,req.body);
-
-//         if(!result){
-//             return res.status(404).json({message:'Book not found :('});
-//         }
-//         return res.status(200).json({message:'Book updated successfuly !!!'});
-//     }
-//     catch(error){
-//         console.log(error);
-//         res.status(500).send({message: error.message});
-//     }
-// });
-
-// //this route is to delete an existing book
-// app.delete('/books/:id',async(req,res)=>{
-//     try{
-        
-//         const {id}=req.params;
-//         const result=await Book.findByIdAndDelete(id);
-
-//         if(!result){
-//             return res.status(404).json({message:'Book not found :('});
-//         }
-//         return res.status(200).json({message:'Book deleted successfuly !!!'});
-//     }
-//     catch(error){
-//         console.log(error);
-//         res.status(404).send({message: error.message});
-//     }
-// });
+//MIDDLEWARE for handling CORS policy
+//ALLOW CUSTOM ORIGINS
+app.use(
+    cors({
+        origin:`http://localhost:5555/books`,
+        methods:['GET','POST','PUT','DELETE'],
+        allowHeaders: ['Content-Type'],
+    })
+);
 
 mongoose
 .connect(MongoDBURL) 
