49907998d65a8217421073dce471967b2c8792e5
Sajal <sajalchandra124@gmail.com>
2024-07-22 23:50:13 +0530

    makign frontend routing

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
-
-
+import {Route,Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import CreateBook from './pages/CreateBook';
+import EditBook from './pages/CreateBook';
+import ShowBook from './pages/ShowBook';
+import DeleteBook from './pages/DeleteBook'
 const App = () => {
   return (
-    <div className='bg-cyan-200'>App</div>
+    <Routes>
+      <Route path='./pages/Home' element={<Home />}/>      
+      <Route path='./pages/CreateBook' element={<CreateBook/>}/>      
+      <Route path='./pages/ShowBook' element={<ShowBook/>}/>      
+      <Route path='./pages/DeleteBook' element={<DeleteBook/>}/>      
+      <Route path='./pages/EditBook' element={<EditBook/>}/>      
+    </Routes>
   )
 }
 
